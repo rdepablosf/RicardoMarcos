301aa56d8ec599ee87003b45ee3bf302be0194e9
HeDichoQueNo <no@nomail.no>
2025-07-17 19:27:00 +0200

    Se implementan métodos personalizados en repository

--- src/main/java/es/cic/curso25/RicardoMarcos/repository/MateriaAprendizajeRepository.java
+++ src/main/java/es/cic/curso25/RicardoMarcos/repository/MateriaAprendizajeRepository.java
@@ -1,9 +1,15 @@
 package es.cic.curso25.RicardoMarcos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.cic.curso25.RicardoMarcos.model.MateriaAprendizaje;
-
+//JpaRepository es una interfaz que ya incluye todas las operaciones CRUD básicas lo implementamos en nuestro proyecto para poder tener acceso a ellas.
 public interface MateriaAprendizajeRepository extends JpaRepository<MateriaAprendizaje, Long> {
+
+    List <MateriaAprendizaje> findByFinalizado(boolean finalizado); //Sentencia que nos traducida a sql devolverá segun finalizado sea true/ false
+
+    MateriaAprendizaje findByNombre(String nombre); //Nos permite buscar por el nombre 
 
 }
