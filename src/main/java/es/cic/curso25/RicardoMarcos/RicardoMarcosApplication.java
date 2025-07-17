037e3b425d9fbc58ff4725eea99bf42d808b0262
HeDichoQueNo <no@nomail.no>
2025-07-17 17:50:54 +0200

    Primer commit solo se crean carpetas de proyecto

---
+++ src/main/java/es/cic/curso25/RicardoMarcos/RicardoMarcosApplication.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.RicardoMarcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicardoMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicardoMarcosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/curso25/RicardoMarcos/RicardoMarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.RicardoMarcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicardoMarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
