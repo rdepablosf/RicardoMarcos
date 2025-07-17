0d551ccb4e8ff45e12d6c91f751b85a7c9a4e3bd
HeDichoQueNo <no@nomail.no>
2025-07-17 19:07:57 +0200

    Se añade repositorio vacio

---
+++ src/main/java/es/cic/curso25/RicardoMarcos/repository/MateriaAprendizajeRepository.java
@@ -0,0 +1,9 @@
+package es.cic.curso25.RicardoMarcos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.cic.curso25.RicardoMarcos.model.MateriaAprendizaje;
+
+public interface MateriaAprendizajeRepository extends JpaRepository<MateriaAprendizaje, Long> {
+
+}
