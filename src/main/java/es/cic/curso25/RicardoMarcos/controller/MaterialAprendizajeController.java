9b89cb2e2d10ad1b35649edf6120992da1a9c087
rdpablos <rdpablos@formacion.cic.es>
2025-07-18 14:00:54 +0200

    metodo toString, carpetas de service y controller

---
+++ src/main/java/es/cic/curso25/RicardoMarcos/controller/MaterialAprendizajeController.java
@@ -0,0 +1,5 @@
+package es.cic.curso25.RicardoMarcos.controller;
+
+public class MaterialAprendizajeController {
+
+}
--- src/main/java/es/cic/curso25/RicardoMarcos/repository/MateriaAprendizajeRepository.java
+++ src/main/java/es/cic/curso25/RicardoMarcos/repository/MateriaAprendizajeRepository.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.cic.curso25.RicardoMarcos.model.MateriaAprendizaje;
+
 //JpaRepository es una interfaz que ya incluye todas las operaciones CRUD básicas lo implementamos en nuestro proyecto para poder tener acceso a ellas.
 public interface MateriaAprendizajeRepository extends JpaRepository<MateriaAprendizaje, Long> {
 
-    List <MateriaAprendizaje> findByFinalizado(boolean finalizado); //Sentencia que nos traducida a sql devolverá segun finalizado sea true/ false
+    List<MateriaAprendizaje> findByFinalizado(boolean finalizado); // Sentencia que nos traducida a sql devolverá segun
+                                                                   // finalizado sea true/ false
 
-    MateriaAprendizaje findByNombre(String nombre); //Nos permite buscar por el nombre 
+    MateriaAprendizaje findByNombre(String nombre); // Nos permite buscar por el nombre
 
 }
