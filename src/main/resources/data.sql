2972280af9706d4d82636a43a7a8ca8d3bfdaa3b
HeDichoQueNo <no@nomail.no>
2025-07-20 10:49:48 +0200

    se añade data para que cargue automaticamente datos de prueba

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO MATERIA_APRENDIZAJE (nombre, finalizado) VALUES ('Java Básico', false);
+INSERT INTO MATERIA_APRENDIZAJE (nombre, finalizado) VALUES ('Spring Boot', true);
+INSERT INTO MATERIA_APRENDIZAJE (nombre, finalizado) VALUES ('Hibernate Avanzado', false);
